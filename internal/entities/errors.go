5b078a0253bd94b29183b6558f161e3f40ba149c
Artem Shahov <shahov9ad@gmail.com>
2024-12-31 08:27:58 +0300

    errors

--- internal/entities/errors.go
+++ internal/entities/errors.go
@@ -5,6 +5,17 @@ import error
 var (
 	//ошибки постов
 	ErrPostNotFound = error.new("post not found")
+	
+	ErrPostNameConflict = error.new("post name conflict")
 
 	//ошибки пользователя
+	ErrUserNotFound = error.new("user not found")
+
+	ErrUserAlreadyExist = error.new("user already exists")
+	
+	ErrUserNotAdmin	= error.new("user not admin")
+	
+	ErrUserNameConflict = error.new("user name conflict")
+	ErrUserEmailConflict = error.new("user email conflict")
+	ErrUserPasswordConflict = error.new("user password conflict")
 )
