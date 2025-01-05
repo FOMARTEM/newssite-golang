6db3338857089daac17294c5f22e08b4358c8cf1
Artem Shahov <shahov9ad@gmail.com>
2025-01-05 09:04:43 +0300

    api interface

---
+++ internal/api/interface.go
@@ -0,0 +1,22 @@
+package api
+
+import "github.com/FOMARTEM/newssite-golang/internal/entities"
+
+type Usecase interface {
+	//работа с User
+	CreateUser(user entities.User) (*entities.User, error)
+	CheckPasswordUser(user entities.User) (*bool, error)
+	SelectUserByID(id int) (*entities.User, error)
+	SelectUserByEmail(email string) (*entities.User, error)
+	UpdateAdminRules(email string, admin bool) (*bool, error)
+	UpdateUser(user entities.User) (*entities.User, error)
+	DeleteUserById(id int) error
+	DeleteUserByEmail(email string) error
+
+	//работа с Post
+	CreatePost(post entities.Post) (*entities.Post, error)
+	SelectPost(id int) (*entities.Post, error)
+	ListPosts() ([]*entities.Post, error)
+	UpdatePost(post entities.Post) (*entities.Post, error)
+	DeletePost(id int) error
+}
