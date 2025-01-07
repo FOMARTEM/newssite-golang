389131e54e21caa0792b9899b99629dff088ebc6
Artem Shahov <shahov9ad@gmail.com>
2025-01-07 10:06:43 +0300

    delete validator

--- internal/entities/post.go
+++ internal/entities/post.go
@@ -2,8 +2,8 @@ package entities
 
 type Post struct {
 	ID         int    `json:"id,omitempty"`
-	Name       string `json:"name" validate:"required,min=10,max=255"`
-	Text       string `json:"text"`
+	Name       string `json:"title"`
+	Text       string `json:"body"`
 	CreateDate string `json:"createdate"`
 	UpdateDate string `json:"updatedate"`
 	UserId     int    `json:"userid"`
