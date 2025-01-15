55fd16f0b12d2dfc899725f0c25b3987b082f135
Shahov Artem <shahov9ad@gmail.com>
2025-01-15 11:10:06 +0300

    edit post struct

--- internal/entities/post.go
+++ internal/entities/post.go
@@ -2,9 +2,10 @@ package entities
 
 type Post struct {
 	ID         int    `json:"id,omitempty"`
-	Name       string `json:"title"`
-	Text       string `json:"body"`
+	Name       string `json:"title" validate:"required,min=3,max=255"`
+	Text       string `json:"body"  validate:"required,min=50"`
 	CreateDate string `json:"createdate"`
-	UpdateDate string `json:"updatedate"`
+	UpdateDate string `json:"updatedate,omitempty"`
 	UserId     int    `json:"userid"`
+	UserName   string `json:"username"`
 }
