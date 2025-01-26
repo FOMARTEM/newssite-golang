b9158b660517d63c08cc95567934d5faff1f2c0c
Shahov Artem <shahov9ad@gmail.com>
2025-01-26 16:21:52 +0300

    usecase for commetns

---
+++ internal/usecase/comments.go
@@ -0,0 +1,57 @@
+package usecase
+
+import (
+	"github.com/FOMARTEM/newssite-golang/internal/entities"
+)
+
+func (u *Usecase) CreateComment(comment entities.Comment) (*entities.Comment, error) {
+	createdComment, err := u.p.InsertComment(comment)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return createdComment, nil
+}
+
+func (u *Usecase) PostComments(post_id int) ([]*entities.Comment, error) {
+	comments, err := u.p.GetCommentsForPost(post_id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return comments, nil
+}
+
+func (u *Usecase) UpdateComment(comment entities.Comment) (*entities.Comment, error) {
+	newComment, err := u.p.UpdateComment(comment)
+
+	if err != nil {
+		return nil, err
+	} else if newComment == nil {
+		return newComment, entities.ErrCommentNotFound
+	}
+
+	return newComment, nil
+}
+
+func (u *Usecase) DeleteComment(id int) error {
+	err := u.p.DeleteComment(id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *Usecase) DeleteCommentsInPost(post_id int) error {
+	err := u.p.DeleteCommentsInPost(post_id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
