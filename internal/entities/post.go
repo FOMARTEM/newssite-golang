aef77abd0ee3de89475bd45ca28da8eebf45a88e
Shahov Artem <shahov9ad@gmail.com>
2025-01-27 18:39:24 +0300

    some updates

--- internal/entities/post.go
+++ internal/entities/post.go
@@ -4,8 +4,8 @@ type Post struct {
 	ID         int    `json:"id,omitempty"`
 	Name       string `json:"title" validate:"required,min=3,max=255"`
 	Text       string `json:"body"  validate:"required,min=50"`
-	CreateDate string `json:"createdate"`
+	CreateDate string `json:"createdate,omitempty"`
 	UpdateDate string `json:"updatedate,omitempty"`
-	UserId     int    `json:"userid"`
-	UserName   string `json:"username"`
+	UserId     int    `json:"userid,omitempty"`
+	UserName   string `json:"username,omitempty"`
 }
