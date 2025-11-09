8541e01453a5dbb19b12b2d37e6edebe0f414ae5
Shahov Artem <shahov9ad@gmail.com>
2025-11-09 19:20:06 +0700

    fix post hide

--- internal/provider/post.go
+++ internal/provider/post.go
@@ -36,7 +36,7 @@ func (p *Provider) SelectPostById(id int) (*entities.Post, error) {
 	err := p.conn.QueryRow(
 		"SELECT * FROM get_post($1)",
 		id,
-	).Scan(&post.ID, &post.Name, &post.Text, &post.CreateDate, &post.UpdateDate, &post.UserId, &post.UserName)
+	).Scan(&post.ID, &post.Name, &post.Text, &post.CreateDate, &post.UpdateDate, &post.Hide, &post.UserId, &post.UserName)
 	if err != nil {
 		return nil, err
 	}
@@ -62,7 +62,7 @@ func (p *Provider) SelectAllPosts() ([]*entities.Post, error) {
 
 	for rows.Next() {
 		var post entities.Post
-		if err := rows.Scan(&post.ID, &post.Name, &post.Text, &post.CreateDate, &post.UpdateDate, &post.UserId, &post.UserName); err != nil {
+		if err := rows.Scan(&post.ID, &post.Name, &post.Text, &post.CreateDate, &post.UpdateDate, &post.Hide, &post.UserId, &post.UserName); err != nil {
 			return nil, err
 		}
 		posts = append(posts, &post)
@@ -90,7 +90,7 @@ func (p *Provider) SelectUserPosts(userId int) ([]*entities.Post, error) {
 
 	for rows.Next() {
 		var post entities.Post
-		if err := rows.Scan(&post.ID, &post.Name, &post.Text, &post.CreateDate, &post.UpdateDate, &post.UserId, &post.UserName); err != nil {
+		if err := rows.Scan(&post.ID, &post.Name, &post.Text, &post.CreateDate, &post.UpdateDate, &post.Hide, &post.UserId, &post.UserName); err != nil {
 			return nil, err
 		}
 		posts = append(posts, &post)
