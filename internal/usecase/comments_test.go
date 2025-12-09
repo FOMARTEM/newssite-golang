25f5399bf9b0dc743d0263889f02ac721873b9f3
Diana Kosmun <dianakosmun@yandex.ru>
2025-12-09 18:13:30 +0700

    update mocks and tests for pagination

--- internal/usecase/comments_test.go
+++ internal/usecase/comments_test.go
@@ -1,178 +1,179 @@
 package usecase_test
 
 import (
-	"errors"
-	"testing"
+  "errors"
+  "testing"
 
-	"github.com/FOMARTEM/newssite-golang/internal/provider/mocks"
-	"github.com/FOMARTEM/newssite-golang/internal/usecase"
-	"github.com/stretchr/testify/assert"
+  "github.com/FOMARTEM/newssite-golang/internal/provider/mocks"
+  "github.com/FOMARTEM/newssite-golang/internal/usecase"
+  "github.com/stretchr/testify/assert"
 
-	"github.com/FOMARTEM/newssite-golang/internal/entities"
+  "github.com/FOMARTEM/newssite-golang/internal/entities"
 )
 
 func TestUsecase_CreateComment(t *testing.T) {
-	mockP := new(mocks.ProviderMock)
-	u := usecase.NewUsecase(mockP)
+  mockP := new(mocks.ProviderMock)
+  u := usecase.NewUsecase(mockP)
 
-	comment := entities.Comment{
-		ID:          1,
-		CommentText: "Nice post",
-		PostId:      10,
-		UserId:      5,
-	}
+  comment := entities.Comment{
+    ID:          1,
+    CommentText: "Nice post",
+    PostId:      10,
+    UserId:      5,
+  }
 
-	t.Run("error while checking user rules", func(t *testing.T) {
-		mockP.On("SelectUserRulesById", 5).Return(nil, errors.New("db error"))
+  t.Run("error while checking user rules", func(t *testing.T) {
+    mockP.On("SelectUserRulesById", 5).Return(nil, errors.New("db error"))
 
-		res, err := u.CreateComment(comment)
+    res, err := u.CreateComment(comment)
 
-		assert.Nil(t, res)
-		assert.EqualError(t, err, "db error")
-	})
+    assert.Nil(t, res)
+    assert.EqualError(t, err, "db error")
+  })
 
-	t.Run("user rules too low", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
-		rules := 0
+  t.Run("user rules too low", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
+    rules := 0
 
-		mockP.On("SelectUserRulesById", 5).Return(&rules, nil)
+    mockP.On("SelectUserRulesById", 5).Return(&rules, nil)
 
-		res, err := u.CreateComment(comment)
+    res, err := u.CreateComment(comment)
 
-		assert.Nil(t, res)
-		assert.Equal(t, entities.ErrCommentNotFound, err)
-	})
+    assert.Nil(t, res)
+    assert.Equal(t, entities.ErrCommentNotFound, err)
+  })
 
-	t.Run("success", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
-		rules := 2
+  t.Run("success", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
+    rules := 2
 
-		mockP.On("SelectUserRulesById", 5).Return(&rules, nil)
-		mockP.On("InsertComment", comment).Return(&comment, nil)
+    mockP.On("SelectUserRulesById", 5).Return(&rules, nil)
+    mockP.On("InsertComment", comment).Return(&comment, nil)
 
-		res, err := u.CreateComment(comment)
+    res, err := u.CreateComment(comment)
 
-		assert.NoError(t, err)
-		assert.Equal(t, &comment, res)
-	})
+    assert.NoError(t, err)
+    assert.Equal(t, &comment, res)
+  })
 
-	t.Run("insert error", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
-		rules := 3
+  t.Run("insert error", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
+    rules := 3
 
-		mockP.On("SelectUserRulesById", 5).Return(&rules, nil)
-		mockP.On("InsertComment", comment).Return(nil, errors.New("insert failed"))
+    mockP.On("SelectUserRulesById", 5).Return(&rules, nil)
+    mockP.On("InsertComment", comment).Return(nil, errors.New("insert failed"))
 
-		res, err := u.CreateComment(comment)
+    res, err := u.CreateComment(comment)
 
-		assert.Nil(t, res)
-		assert.EqualError(t, err, "insert failed")
-	})
+    assert.Nil(t, res)
+    assert.EqualError(t, err, "insert failed")
+  })
 }
 
 func TestUsecase_PostComments(t *testing.T) {
-	mockP := new(mocks.ProviderMock)
-	u := usecase.NewUsecase(mockP)
+  mockP := new(mocks.ProviderMock)
+  u := usecase.NewUsecase(mockP)
 
-	comments := []*entities.Comment{
-		{ID: 1, CommentText: "Hello"},
-	}
+  comments := []*entities.Comment{
+    {ID: 1, CommentText: "Hello"},
+  }
 
-	t.Run("success", func(t *testing.T) {
-		mockP.On("GetCommentsForPost", 10).Return(comments, nil)
+  t.Run("success", func(t *testing.T) {
+    mockP.On("GetCommentsForPost", 10).Return(comments, nil)
 
-		res, err := u.PostComments(10)
+    res, err := u.PostComments(10, 1, 1)
 
-		assert.NoError(t, err)
-		assert.Equal(t, comments, res)
-	})
+    assert.NoError(t, err)
+    assert.Equal(t, comments, res)
+  })
 
-	t.Run("db error", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
-		mockP.On("GetCommentsForPost", 20).Return(nil, errors.New("db error"))
+  t.Run("db error", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
+    mockP.On("GetCommentsForPost", 20).Return(nil, errors.New("db error"))
 
-		res, err := u.PostComments(20)
+    res, err := u.PostComments(20, 1, 1)
 
-		assert.Nil(t, res)
-		assert.EqualError(t, err, "db error")
-	})
+    assert.Nil(t, res)
+    assert.EqualError(t, err, "db error")
+  })
 }
 func TestUsecase_UpdateComment(t *testing.T) {
-	mockP := new(mocks.ProviderMock)
-	u := usecase.NewUsecase(mockP)
+  mockP := new(mocks.ProviderMock)
+  u := usecase.NewUsecase(mockP)
 
-	comment := entities.Comment{ID: 1, CommentText: "Edit"}
+  comment := entities.Comment{ID: 1, CommentText: "Edit"}
 
-	t.Run("db error", func(t *testing.T) {
-		mockP.On("UpdateComment", comment).Return(nil, errors.New("update error"))
+  t.Run("db error", func(t *testing.T) {
+    mockP.On("UpdateComment", comment).Return(nil, errors.New("update error"))
 
-		res, err := u.UpdateComment(comment)
+    res, err := u.UpdateComment(comment)
 
-		assert.Nil(t, res)
-		assert.EqualError(t, err, "update error")
-	})
+    assert.Nil(t, res)
+    assert.EqualError(t, err, "update error")
+  })
 
-	t.Run("comment not found", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
-		mockP.On("UpdateComment", comment).Return(nil, nil)
+  t.Run("comment not found", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
+    mockP.On("UpdateComment", comment).Return(nil, nil)
 
-		res, err := u.UpdateComment(comment)
+    res, err := u.UpdateComment(comment)
 
-		assert.Nil(t, res)
-		assert.Equal(t, entities.ErrCommentNotFound, err)
-	})
+    assert.Nil(t, res)
+    assert.Equal(t, entities.ErrCommentNotFound, err)
+  })
 
-	t.Run("success", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
-		updated := &entities.Comment{ID: 1, CommentText: "Updated"}
+  t.Run("success", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
+    updated := &entities.Comment{ID: 1, CommentText: "Updated"}
 
-		mockP.On("UpdateComment", comment).Return(updated, nil)
+    mockP.On("UpdateComment", comment).Return(updated, nil)
 
-		res, err := u.UpdateComment(comment)
+    res, err := u.UpdateComment(comment)
 
-		assert.NoError(t, err)
-		assert.Equal(t, updated, res)
-	})
+    assert.NoError(t, err)
+    assert.Equal(t, updated, res)
+  })
 }
 func TestUsecase_DeleteComment(t *testing.T) {
-	mockP := new(mocks.ProviderMock)
-	u := usecase.NewUsecase(mockP)
+  mockP := new(mocks.ProviderMock)
+  u := usecase.NewUsecase(mockP)
 
-	t.Run("success", func(t *testing.T) {
-		mockP.On("DeleteComment", 1).Return(nil)
+  t.Run("success", func(t *testing.T) {
+    mockP.On("DeleteComment", 1).Return(nil)
 
-		err := u.DeleteComment(1)
+    err := u.DeleteComment(1)
 
-		assert.NoError(t, err)
-	})
+    assert.NoError(t, err)
+  })
 
-	t.Run("error", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
-		mockP.On("DeleteComment", 2).Return(errors.New("db error"))
+  t.Run("error", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
+    mockP.On("DeleteComment", 2).Return(errors.New("db error"))
 
-		err := u.DeleteComment(2)
+    err := u.DeleteComment(2)
 
-		assert.EqualError(t, err, "db error")
-	})
+    assert.EqualError(t, err, "db error")
+  })
 }
 func TestUsecase_DeleteCommentsInPost(t *testing.T) {
-	mockP := new(mocks.ProviderMock)
-	u := usecase.NewUsecase(mockP)
+  mockP := new(mocks.ProviderMock)
+  u := usecase.NewUsecase(mockP)
 
-	t.Run("success", func(t *testing.T) {
-		mockP.On("DeleteCommentsInPost", 10).Return(nil)
+  t.Run("success", func(t *testing.T) {
+    mockP.On("DeleteCommentsInPost", 10).Return(nil)
 
-		err := u.DeleteCommentsInPost(10)
 
-		assert.NoError(t, err)
-	})
+    err := u.DeleteCommentsInPost(10)
 
-	t.Run("error", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
-		mockP.On("DeleteCommentsInPost", 20).Return(errors.New("err delete"))
+    assert.NoError(t, err)
+  })
 
-		err := u.DeleteCommentsInPost(20)
+  t.Run("error", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
+    mockP.On("DeleteCommentsInPost", 20).Return(errors.New("err delete"))
 
-		assert.EqualError(t, err, "err delete")
-	})
+    err := u.DeleteCommentsInPost(20)
+
+    assert.EqualError(t, err, "err delete")
+  })
 }
--- internal/usecase/posts_test.go
+++ internal/usecase/posts_test.go
@@ -1,199 +1,200 @@
 package usecase_test
 
 import (
-	"testing"
+  "testing"
 
-	"github.com/FOMARTEM/newssite-golang/internal/provider/mocks"
-	"github.com/FOMARTEM/newssite-golang/internal/usecase"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
+  "github.com/FOMARTEM/newssite-golang/internal/provider/mocks"
+  "github.com/FOMARTEM/newssite-golang/internal/usecase"
+  "github.com/stretchr/testify/assert"
+  "github.com/stretchr/testify/mock"
 
-	"github.com/FOMARTEM/newssite-golang/internal/entities"
+  "github.com/FOMARTEM/newssite-golang/internal/entities"
 )
 
 func TestCreatePost(t *testing.T) {
-	mockP := new(mocks.ProviderMock)
-	u := usecase.NewUsecase(mockP)
+  mockP := new(mocks.ProviderMock)
+  u := usecase.NewUsecase(mockP)
 
-	post := entities.Post{UserId: 1, Name: "Test", Text: "Lorem ipsum dolor sit amet...."}
+  post := entities.Post{UserId: 1, Name: "Test", Text: "Lorem ipsum dolor sit amet...."}
 
-	t.Run("access denied", func(t *testing.T) {
-		rules := 3
-		mockP.On("SelectUserRulesById", post.UserId).Return(&rules, nil)
+  t.Run("access denied", func(t *testing.T) {
+    rules := 3
+    mockP.On("SelectUserRulesById", post.UserId).Return(&rules, nil)
 
-		res, err := u.CreatePost(post)
-		assert.Nil(t, res)
-		assert.Equal(t, entities.ErrAccessDenied, err)
-	})
+    res, err := u.CreatePost(post)
+    assert.Nil(t, res)
+    assert.Equal(t, entities.ErrAccessDenied, err)
+  })
 
-	t.Run("success", func(t *testing.T) {
-		rules := 7
-		mockP.ExpectedCalls = nil
-		mockP.On("SelectUserRulesById", post.UserId).Return(&rules, nil)
-		mockP.On("InsertPost", post).Return(&post, nil)
+  t.Run("success", func(t *testing.T) {
+    rules := 7
+    mockP.ExpectedCalls = nil
+    mockP.On("SelectUserRulesById", post.UserId).Return(&rules, nil)
+    mockP.On("InsertPost", post).Return(&post, nil)
 
-		res, err := u.CreatePost(post)
-		assert.NoError(t, err)
-		assert.Equal(t, &post, res)
-	})
+    res, err := u.CreatePost(post)
+    assert.NoError(t, err)
+    assert.Equal(t, &post, res)
+  })
 }
 
 func TestSelectPost(t *testing.T) {
-	mockP := new(mocks.ProviderMock)
-	u := usecase.NewUsecase(mockP)
+  mockP := new(mocks.ProviderMock)
+  u := usecase.NewUsecase(mockP)
 
-	post := &entities.Post{ID: 10}
+  post := &entities.Post{ID: 10}
 
-	t.Run("not found", func(t *testing.T) {
-		mockP.On("SelectPostById", 10).Return(nil, nil)
+  t.Run("not found", func(t *testing.T) {
+    mockP.On("SelectPostById", 10).Return(nil, nil)
 
-		res, err := u.SelectPost(10)
-		assert.Nil(t, res)
-		assert.Equal(t, entities.ErrPostNotFound, err)
-	})
+    res, err := u.SelectPost(10)
+    assert.Nil(t, res)
+    assert.Equal(t, entities.ErrPostNotFound, err)
+  })
 
-	t.Run("success", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
-		mockP.On("SelectPostById", 10).Return(post, nil)
+  t.Run("success", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
+    mockP.On("SelectPostById", 10).Return(post, nil)
 
-		res, err := u.SelectPost(10)
-		assert.NoError(t, err)
-		assert.Equal(t, post, res)
-	})
+    res, err := u.SelectPost(10)
+    assert.NoError(t, err)
+    assert.Equal(t, post, res)
+  })
 }
 
 func TestListPosts(t *testing.T) {
-	mockP := new(mocks.ProviderMock)
-	u := usecase.NewUsecase(mockP)
+  mockP := new(mocks.ProviderMock)
+  u := usecase.NewUsecase(mockP)
 
-	posts := []*entities.Post{{ID: 1}}
+  posts := []*entities.Post{{ID: 1}}
 
-	mockP.On("SelectAllPosts").Return(posts, nil)
+  mockP.On("SelectAllPosts").Return(posts, nil)
 
-	res, err := u.ListPosts()
-	assert.NoError(t, err)
-	assert.Equal(t, posts, res)
+  res, err := u.ListPosts(1, 1)
+  assert.NoError(t, err)
+  assert.Equal(t, posts, res)
 }
 
 func TestListUserPosts(t *testing.T) {
-	mockP := new(mocks.ProviderMock)
-	u := usecase.NewUsecase(mockP)
+  mockP := new(mocks.ProviderMock)
+  u := usecase.NewUsecase(mockP)
 
-	posts := []*entities.Post{{ID: 1}}
+  posts := []*entities.Post{{ID: 1}}
 
-	mockP.On("SelectUserPosts", 5).Return(posts, nil)
+  mockP.On("SelectUserPosts", 5).Return(posts, nil)
 
-	res, err := u.ListUserPosts(5)
-	assert.NoError(t, err)
-	assert.Equal(t, posts, res)
+  res, err := u.ListUserPosts(5, 1, 1)
+  assert.NoError(t, err)
+  assert.Equal(t, posts, res)
 }
 
 func TestUpdatePost(t *testing.T) {
-	mockP := new(mocks.ProviderMock)
-	u := usecase.NewUsecase(mockP)
+  mockP := new(mocks.ProviderMock)
+  u := usecase.NewUsecase(mockP)
 
-	post := entities.Post{ID: 1}
-	dbPost := &entities.Post{ID: 1, UserId: 7}
+  post := entities.Post{ID: 1}
+  dbPost := &entities.Post{ID: 1, UserId: 7}
 
-	t.Run("wrong owner", func(t *testing.T) {
-		mockP.On("SelectPostById", 1).Return(dbPost, nil)
+  t.Run("wrong owner", func(t *testing.T) {
+    mockP.On("SelectPostById", 1).Return(dbPost, nil)
 
-		res, err := u.UpdatePost(post, 999)
-		assert.Nil(t, res)
-		assert.Equal(t, entities.ErrPostNotFound, err)
-	})
+    res, err := u.UpdatePost(post, 999)
+    assert.Nil(t, res)
+    assert.Equal(t, entities.ErrPostNotFound, err)
+  })
 
-	t.Run("access denied", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
-		rules := 2
+  t.Run("access denied", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
+    rules := 2
 
-		mockP.On("SelectPostById", 1).Return(dbPost, nil)
-		mockP.On("SelectUserRulesById", 7).Return(&rules, nil)
+    mockP.On("SelectPostById", 1).Return(dbPost, nil)
+    mockP.On("SelectUserRulesById", 7).Return(&rules, nil)
 
-		res, err := u.UpdatePost(post, 7)
-		assert.Nil(t, res)
-		assert.Equal(t, entities.ErrAccessDenied, err)
-	})
+    res, err := u.UpdatePost(post, 7)
+    assert.Nil(t, res)
+    assert.Equal(t, entities.ErrAccessDenied, err)
+  })
 
-	t.Run("success", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
+  t.Run("success", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
 
-		rules := 7
-		updated := &entities.Post{ID: 1, Name: "AAA"}
+    rules := 7
+    updated := &entities.Post{ID: 1, Name: "AAA"}
 
-		mockP.On("SelectPostById", 1).Return(dbPost, nil)
-		mockP.On("SelectUserRulesById", 7).Return(&rules, nil)
-		mockP.On("UpdatePostById", post).Return(updated, nil)
+    mockP.On("SelectPostById", 1).Return(dbPost, nil)
+    mockP.On("SelectUserRulesById", 7).Return(&rules, nil)
+    mockP.On("UpdatePostById", post).Return(updated, nil)
 
-		res, err := u.UpdatePost(post, 7)
-		assert.NoError(t, err)
-		assert.Equal(t, updated, res)
-	})
+    res, err := u.UpdatePost(post, 7)
+    assert.NoError(t, err)
+    assert.Equal(t, updated, res)
+  })
 }
 
 func TestHidePost(t *testing.T) {
-	mockP := new(mocks.ProviderMock)
-	u := usecase.NewUsecase(mockP)
+  mockP := new(mocks.ProviderMock)
+  u := usecase.NewUsecase(mockP)
 
-	post := &entities.Post{ID: 1, Hide: 0}
+  post := &entities.Post{ID: 1, Hide: 0}
 
-	t.Run("access denied", func(t *testing.T) {
-		rules := 3
-		mockP.On("SelectUserRulesById", 5).Return(&rules, nil)
+  t.Run("access denied", func(t *testing.T) {
+    rules := 3
+    mockP.On("SelectUserRulesById", 5).Return(&rules, nil)
 
-		err := u.HidePost(1, 5)
-		assert.Equal(t, entities.ErrAccessDenied, err)
-	})
+    err := u.HidePost(1, 5)
+    assert.Equal(t, entities.ErrAccessDenied, err)
+  })
 
-	t.Run("success hide/unhide", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
-		rules := 7
+  t.Run("success hide/unhide", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
+    rules := 7
 
-		mockP.On("SelectUserRulesById", 5).Return(&rules, nil)
-		mockP.On("SelectPostById", 1).Return(post, nil)
-		mockP.On("EditVisibilityById", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+    mockP.On("SelectUserRulesById", 5).Return(&rules, nil)
+    mockP.On("SelectPostById", 1).Return(post, nil)
+    mockP.On("EditVisibilityById", mock.Anything, mock.Anything, mock.Anything).Return(nil)
 
-		err := u.HidePost(1, 5)
-		assert.NoError(t, err)
-	})
+    err := u.HidePost(1, 5)
+    assert.NoError(t, err)
+  })
 }
 
+
 func TestDeletePost(t *testing.T) {
-	mockP := new(mocks.ProviderMock)
-	u := usecase.NewUsecase(mockP)
+  mockP := new(mocks.ProviderMock)
+  u := usecase.NewUsecase(mockP)
 
-	dbPost := &entities.Post{ID: 10, UserId: 7}
+  dbPost := &entities.Post{ID: 10, UserId: 7}
 
-	t.Run("wrong owner", func(t *testing.T) {
-		mockP.On("SelectPostById", 10).Return(dbPost, nil)
+  t.Run("wrong owner", func(t *testing.T) {
+    mockP.On("SelectPostById", 10).Return(dbPost, nil)
 
-		err := u.DeletePost(10, 999)
-		assert.Equal(t, entities.ErrPostNotFound, err)
-	})
+    err := u.DeletePost(10, 999)
+    assert.Equal(t, entities.ErrPostNotFound, err)
+  })
 
-	t.Run("access denied", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
+  t.Run("access denied", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
 
-		rules := 1
+    rules := 1
 
-		mockP.On("SelectPostById", 10).Return(dbPost, nil)
-		mockP.On("SelectUserRulesById", 7).Return(&rules, nil)
+    mockP.On("SelectPostById", 10).Return(dbPost, nil)
+    mockP.On("SelectUserRulesById", 7).Return(&rules, nil)
 
-		err := u.DeletePost(10, 7)
-		assert.Equal(t, entities.ErrAccessDenied, err)
-	})
+    err := u.DeletePost(10, 7)
+    assert.Equal(t, entities.ErrAccessDenied, err)
+  })
 
-	t.Run("success", func(t *testing.T) {
-		mockP.ExpectedCalls = nil
+  t.Run("success", func(t *testing.T) {
+    mockP.ExpectedCalls = nil
 
-		rules := 7
+    rules := 7
 
-		mockP.On("SelectPostById", 10).Return(dbPost, nil)
-		mockP.On("SelectUserRulesById", 7).Return(&rules, nil)
-		mockP.On("DeleteCommentsInPost", 10).Return(nil)
-		mockP.On("DeletePostById", 10).Return(nil)
+    mockP.On("SelectPostById", 10).Return(dbPost, nil)
+    mockP.On("SelectUserRulesById", 7).Return(&rules, nil)
+    mockP.On("DeleteCommentsInPost", 10).Return(nil)
+    mockP.On("DeletePostById", 10).Return(nil)
 
-		err := u.DeletePost(10, 7)
-		assert.NoError(t, err)
-	})
+    err := u.DeletePost(10, 7)
+    assert.NoError(t, err)
+  })
 }
